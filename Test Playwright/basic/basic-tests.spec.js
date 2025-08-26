668b498bed496d115728e72245d47f1c50b73f83
Pongpeera Thapdacha <satang7640@gmail.com>
2025-08-26 18:37:27 +0700

    Delete Test Playwright directory

--- Test Playwright/basic/basic-tests.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Home page should load correctly', async ({ page }) => {
-  await page.goto('https://booknest-dango.netlify.app/');
-  await expect(page).toHaveTitle(/BookNest/);
-  await expect(page.locator('header')).toBeVisible();
-});
